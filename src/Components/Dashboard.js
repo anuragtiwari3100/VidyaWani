f5b960fc7ef46388adf61ef840eb9ecdcfccbef9
Anurag Tiwari <anuragtiwarigpj2005@gmail.con>
2024-07-20 12:49:02 +0530

    Commiting Changes For the First Time

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='grid place-items-center text-richblack-100 text-3xl h-full'>Dashboard !!</div>
+
+  )
+}
+
+export default Dashboard
+ 
